c4f3c398036efb564b9ea57b26f106e5ab453c55
unknown <sam.arif1111@gmail.com>
2019-12-10 22:58:52 +0530

    added basic dependencies

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import classes from './App.css';
 
 class App extends Component {
   render() {
+    console.log('props', this.props)
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      
+      <Fragment><div className={classes.App}>
+        <header className={classes.AppHeader}>
+          <img src={logo} className={classes.AppLog} alt="logo" />
+          <h1 className={classes.AppTitle}>Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p className={classes.AppIntro}>
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      </p>
       </div>
+      <p>
+        My name is sameen</p></Fragment>
+
     );
   }
 }
