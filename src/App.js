5435cc089ff4e42f19571270ee890cf36d774e2e
unknown <sam.arif1111@gmail.com>
2019-12-10 23:15:32 +0530

    basic cleanup work

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React, { Component, Fragment } from 'react';
-import logo from './logo.svg';
-import classes from './App.css';
+
 
 class App extends Component {
   render() {
-    console.log('props', this.props)
     return (
       
-      <Fragment><div className={classes.App}>
-        <header className={classes.AppHeader}>
-          <img src={logo} className={classes.AppLog} alt="logo" />
-          <h1 className={classes.AppTitle}>Welcome to React</h1>
-        </header>
-        <p className={classes.AppIntro}>
-          To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-      </div>
-      <p>
-        My name is sameen</p></Fragment>
+      <Fragment>
+
+
+
+      </Fragment>
 
     );
   }
